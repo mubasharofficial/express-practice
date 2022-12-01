e235301158ab344338ae6edd8c9e7f63241e979c
mubashar <mubashar.hussain22@gmail.com>
2022-12-02 02:05:21 +0500

    create src folder

--- prd/index.js
+++ prd/index.js
@@ -8,7 +8,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var app = (0, _express["default"])();
 var port = 3000;
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Pakistan!');
 });
 app.listen(port, function () {
   console.log("Example app listening on port ".concat(port));
