6fafb06a744319c7854cddfcda75fa49f69120f2
mubashar <mubashar.hussain22@gmail.com>
2022-12-10 08:49:37 +0500

    start working on  view

--- app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import router from './routes/student.js'
-const app = express();
-const port = process.env.PORT || '3000'
-// Load Router Modules
-app.use('/student/',router)
-app.listen(port,()=>{
-    console.log(`Server is Listening at http://localhost:${port}`);
-});
--- routes/student.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import {all_student,delete_student} from '../controllers/studentController.js'
-const router = express.Router();
-router.get('/all',all_student);
-router.get('/delete/:id([0-9]{2})',delete_student); // require 2 figure number id like '23,42,52'
-
-export default router;
