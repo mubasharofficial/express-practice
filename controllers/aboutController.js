01def2c615dcfc04ff43d725d948ba0f8d6109f2
mubashar <mubashar.hussain22@gmail.com>
2022-12-10 12:20:15 +0500

    start working on it

--- controllers/aboutController.js
+++
@@ -1,7 +0,0 @@
-import {join} from 'path';
-const aboutController = (req,res)=>{
-    res.sendFile(join(process.cwd(),'views','about.html'));
-    // res.send('About Page');
-}
-
-export {aboutController};
