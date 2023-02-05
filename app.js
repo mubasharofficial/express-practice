6c71071c0d671416e8c91b484a684ab0c1ecddd7
mubashar <mubashar.hussain22@gmail.com>
2023-02-05 18:04:50 +0500

    completed lecture

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import express from 'express';
 import conn from './dbconfig/connect.js';
 const app = express();
 const port = process.env.PORT || 3000;
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/school"
+// const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://root:toor@127.0.0.1:27017/school?authSource=school';
+const DATABASE_URL = process.env.DB_HOST +':'+ process.env.PORT  || 'mongodb://127.0.0.1:27017';
 
 // mongoose.set("strictQuery", false);
-// mongoose.connect('mongodb://127.0.0.1:27017/school')
+// mongoose.connect(DATABASE_URL)
 // .then(()=>{
 //     console.log('mongo connected successfully')
 // })
@@ -14,7 +15,7 @@ const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/scho
 //db.createUser({user:'root',pwd:'toor',roles:[{role:'read',db:'school'}]})
 
 (()=>{
-    const connection = conn();
+    const connection = conn(DATABASE_URL);
 })()
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`);
--- dbconfig/connect.js
+++ dbconfig/connect.js
@@ -21,18 +21,24 @@ const LOCAL_HOST = process.env.DB_HOST || '127.0.0.1'
 
 const conn = async(DATABASE_URL)=>{
     mongoose.set("strictQuery", false);
-    // await mongoose.connect(`mongodb://${LOCAL_HOST}:${PORT_NUMBER}/${DATABASE_NAME}`)
-    return  mongoose.connect(DATABASE_URL)
-    .then((conn)=>{
-        console.log("Connect To MongoDb Server successfully");
-        
-    })
-    .catch(
-        (err)=>{
-            console.log(`could not connected to MongoDb: ${err}`);
-           
+    try{
+        // await mongoose.connect(`mongodb://${LOCAL_HOST}:${PORT_NUMBER}/${DATABASE_NAME}`)
+        // return  mongoose.connect("mongodb://root:toor@127.0.0.1:27017/school?authSource=school")
+      const  DB_OPTIONS = {
+            user: process.env.DB_DATABASE || 'root',
+            pass: process.env.DB_PASSWORD || 'toor' ,
+            dbname: process.env.DB_DATABASE || 'school',
+            authSource:process.env.DB_AUTH_SOURCE || 'school'
         }
-    )
+    await mongoose.connect(DATABASE_URL,DB_OPTIONS)
+    console.log("Success fully connect")
+    }
+    catch(err)
+    {
+      console.log(`could not connect due To: ${err}`)
+    }
+  
+    
 
 }
 
