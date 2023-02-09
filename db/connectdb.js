bfae7eb769f33ce4b7253c94e407c98a37f1bf04
mubashar <mubashar.hussain22@gmail.com>
2023-02-09 06:07:33 +0500

    create database connections

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async (DATABASE_URL) =>{
+    mongoose.set('strictQuery', true);
     try{
         const DB_OPTIONS ={
-            dbName: process.env.DATABASE_NAME || 'school',
+            dbName: process.env.DATABASE_NAME || 'school_testdb',  // on start if not exit than create this db
         }
-        await mongoose.connect(DATABASE_URL,DB_OPTIONS)
+        await mongoose.connect(DATABASE_URL,DB_OPTIONS);
+        console.log('connected successfully');
     }
     catch(err)
     {
         console.log(err)
     }
 }
+
+export default connectDB;
